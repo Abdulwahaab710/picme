ea718b4882a889aababac65935ce6dce30f516c8
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-01-21 16:44:19 -0500

    added image

--- react-native/src/containers/Home.js
+++ react-native/src/containers/Home.js
@@ -2,9 +2,16 @@ import React from 'react';
 import{ Image } from 'react-native';
 
 export default class Home extends React.Component {
+
+    static get defaultProps(){
+        return{
+            title: 'Home'
+        };
+    }
+
     render(){
         return (
-            <Image source={require('https://facebook.github.io/react/img/logo_og.png')}
+            <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
                    style={{width: 400, height: 400}}/>
         )
     }
