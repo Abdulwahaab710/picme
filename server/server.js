21dfcd599a16d125b155494dfaa30e058f6ad7f5
Aaron Persaud <aaronpersaud@hotmail.com>
2017-01-21 20:04:20 -0500

    mongodb connection is only made once

--- server/server.js
+++ server/server.js
@@ -8,7 +8,15 @@ var url = 'mongodb://64.137.168.9:27017/PicMe';
 
 app.use(bodyParser())
 
-
+//Make MongoDB Connection (DO NOT END: Joseph's orders!!)
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+  }
+});
 // var find = function(db,callback) {
 // 	var collection = db.collection('Evsesent');
 // 	console.log(collection)
@@ -17,21 +25,19 @@ app.use(bodyParser())
 // 	});
 // }
 // Use connect method to connect to the Server
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    //HURRAY!! We are connected. :)
-    console.log('Connection established to', url);
-  }
-  	console.log(db)
-  	var collection = db.collection('Event');
-  	console.log(collection.find());
-  	console.log(collection)
 
-    //find(db,function() { 
-    //Close connection
-    db.close();
+// Get the documents collection
+var collection = db.collection('Event');
+
+// EndPoints
+collection.find({}).toArray(function (err, result) {
+  if (err) {
+    console.log(err);
+  } else if (result.length) {
+    console.log('Found:', result);
+  } else {
+    console.log('No document(s) found with defined "find" criteria!');
+  }
 });
 
 app.get('/', function (req, res) {
