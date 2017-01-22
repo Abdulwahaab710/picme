a2edc50bdb3dfb1e71ea15d8eb7a6e1099c8de2d
Aaron Persaud <aaronpersaud@hotmail.com>
2017-01-21 21:59:58 -0500

    created GET event endpoint

--- server/server.js
+++ server/server.js
@@ -3,41 +3,39 @@ var app = express()
 var mongodb = require('mongodb');
 var bodyParser = require('body-parser')
 var MongoClient = mongodb.MongoClient;
+var ObjectId = require('mongodb').ObjectID;
 
 var url = 'mongodb://64.137.168.9:27017/PicMe';
 
 app.use(bodyParser())
 
+var db;
 //Make MongoDB Connection (DO NOT END: Joseph's orders!!)
-MongoClient.connect(url, function (err, db) {
+MongoClient.connect(url, function (err, database) {
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
     //HURRAY!! We are connected. :)
     console.log('Connection established to', url);
+    db = database;
   }
 });
-// var find = function(db,callback) {
-// 	var collection = db.collection('Evsesent');
-// 	console.log(collection)
-// 	collection.find({}).toArray(function(err,docs) {
-// 		console.log(docs);
-// 	});
-// }
-// Use connect method to connect to the Server
 
-// Get the documents collection
-var collection = db.collection('Event');
-
-// EndPoints
-collection.find({}).toArray(function (err, result) {
-  if (err) {
-    console.log(err);
-  } else if (result.length) {
-    console.log('Found:', result);
-  } else {
-    console.log('No document(s) found with defined "find" criteria!');
-  }
+// GET event
+app.get('/events/:id',function(req,res) {
+	var id = req.params.id;
+	//console.log(id)
+	var collection = db.collection('Event');
+	collection.find({"_id" : ObjectId(id)}).toArray(function (err, result) {
+	  if (err) {
+	    res.send(err);
+	  } else if (result.length) {
+	    res.send(result[0]);
+	    console.log("OK")
+	  } else {
+		res.status(404)
+	  }
+	});
 });
 
 var bodyParser = require('body-parser');
