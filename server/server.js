574c183f7bf2c0da2794f0e3f1da93989adc616d
Hirthanan Subenderan <Hirthanan_97@hotmail.com>
2017-01-21 23:40:29 -0500

    post http-req

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ var url = 'mongodb://64.137.168.9:27017/PicMe';
 app.use(bodyParser())
 
 var db;
+
 //Make MongoDB Connection (DO NOT END: Joseph's orders!!)
 MongoClient.connect(url, function (err, database) {
   if (err) {
@@ -20,6 +21,7 @@ MongoClient.connect(url, function (err, database) {
     db = database;
   }
 });
+
 
 // GET event
 app.get('/events/:id',function(req,res) {
@@ -47,11 +49,26 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.post('/upload/photo', function(req, res) {
-    var photo_id = req.body.id;
-    res.send("Post request sucessfully called with photo_id: " + photo_id);
+app.post('/event', function(req, res) {
+    var name = req.body.name;
+    var start_time = req.body.start_time;
+    var end_time = req.body.end_time;
+    var description = req.body.description;
+
+    console.log(name);
+
+    var collection = db.collection('Event');
+    collection.insert({'name': name, 'start_time': start_time, 'end_time': end_time,
+        'description': description}, function(err, event) {
+        // db.close();
+        res.send("OK");
+    });
+
+    // res.send("Post request sucessfully called with values:" + name + start_time + end_time + description);
 });
 
+// app.post('/upload/:photo', function(req, res) {
+// })
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
